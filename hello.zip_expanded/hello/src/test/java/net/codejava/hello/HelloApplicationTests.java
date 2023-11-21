230b922600342c2e1748b102345df0f700a7bb23
Rex Nieto <rnieto>
2023-11-22 06:25:39 +0800

    Initial RESTful with Spring Boot

---
+++ hello.zip_expanded/hello/src/test/java/net/codejava/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
